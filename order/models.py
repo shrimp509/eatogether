4d7d4070806f2fa6730ca5b18d181f92be34b8f3
RongSon <shrimp509@hotmail.com>
2020-05-11 13:39:48 +0800

    Add `Item` model in order app - n `Item` related to 1 `Order`

--- order/models.py
+++ order/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from account.models import User
+from django.utils import timezone
 
 '''
 request_body:
@@ -13,12 +14,45 @@ request_body:
     "deadline": 20
 }
 '''
-class Order(models.Model):
-    creator = models.fields.CharField()
-    title = models.fields.CharField()
-    content = models.fields.CharField()
-    menu_url = models.fields.URLField()
-    max_followers = models.fields.PositiveIntegerField()
-    deadline = models.fields.DateTimeField()
 
-    current_followers = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Order(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.fields.CharField(default="", max_length=50)
+    content = models.fields.CharField(default="", max_length=100, blank=True, null=True)
+    menu_url = models.fields.URLField(default="", blank=True, null=True)
+    max_followers = models.fields.PositiveSmallIntegerField(default=0)
+    deadline = models.fields.DateTimeField(default=timezone.now)
+    created = models.fields.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        if self.deadline > timezone.now():
+            return "由{}發起`{}`, {:.1f}分鐘後收單".format(
+                self.creator.name,
+                self.title,
+                (self.deadline - timezone.now()).seconds / 60
+            )
+        else:
+            return "由{}發起`{}`, 已截單, {}發起, {}截止".format(
+                self.creator.name,
+                self.title,
+                self.created.strftime('%Y-%m-%d %H:%M'),
+                self.deadline.strftime('%Y-%m-%d %H:%M')
+            )
+
+
+class Item(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.fields.CharField(max_length=50)
+    remarks = models.fields.CharField(max_length=100)
+    count = models.fields.PositiveSmallIntegerField(default=1)
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}點了{}個{}，特殊要求{}".format(
+            self.creator,
+            self.product,
+            self.count,
+            self.remarks
+        )
