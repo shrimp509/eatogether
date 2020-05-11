e84788ae1e19ebe46a89a0598ba6be75bacbcd99
RongSon <shrimp509@hotmail.com>
2020-05-11 16:39:02 +0800

    Add is_expired() method for `order` - and Add `created` attribute

--- order/models.py
+++ order/models.py
@@ -25,8 +25,14 @@ class Order(models.Model):
     deadline = models.fields.DateTimeField(default=timezone.now)
     created = models.fields.DateTimeField(default=timezone.now)
 
+    def is_expired(self):
+        print("in is_expired: {}, deadline: {}, now: {}".format(
+            timezone.now() >= self.deadline, self.deadline, timezone.now())
+        )
+        return timezone.now() >= self.deadline
+
     def __str__(self):
-        if self.deadline > timezone.now():
+        if not self.is_expired():
             return "由{}發起`{}`, {:.1f}分鐘後收單".format(
                 self.creator.name,
                 self.title,
@@ -46,6 +52,7 @@ class Item(models.Model):
     product = models.fields.CharField(max_length=50)
     remarks = models.fields.CharField(max_length=100)
     count = models.fields.PositiveSmallIntegerField(default=1)
+    created = models.fields.DateTimeField(default=timezone.now)
 
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
 
